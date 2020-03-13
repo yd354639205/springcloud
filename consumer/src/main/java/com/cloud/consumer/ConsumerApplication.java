b589afe72d032fea6a14debe03b919c45a1f64c8
yangdong <yangdong@casachina.com.cn>
2020-03-13 15:48:46 +0800

    Hystrix

--- consumer/src/main/java/com/cloud/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/cloud/consumer/ConsumerApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 @RestController
 public class ConsumerApplication {
 
--- consumer/src/main/java/com/cloud/consumer/UserController.java
+++ consumer/src/main/java/com/cloud/consumer/UserController.java
@@ -1,5 +1,6 @@
 package com.cloud.consumer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -13,8 +14,14 @@ public class UserController {
         this.restTemplate = restTemplate;
     }
 
+    @HystrixCommand(fallbackMethod = "defaultTest")
     @GetMapping("test")
     public String test() {
         return restTemplate.getForObject("http://SPRING-CLOUD-PRODUCER/hello/user", String.class);
+    }
+
+
+    public String defaultTest() {
+        return "This is an error message";
     }
 }
